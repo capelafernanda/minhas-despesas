9c50ec2d31fdfeddf1d4a9ac1b0b9edbe3d21c8d
capelafernanda <capela.fernand4@gmail.com>
2025-10-10 23:17:47 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,43 @@
-// service-worker.js
-const CACHE_NAME = "minhas-despesas-cache-v1";
+const CACHE_NAME = 'minhas-despesas-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
-  "/manifest.json"
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-// Instala o service worker e adiciona arquivos ao cache
-self.addEventListener("install", event => {
+// Instalando o Service Worker e adicionando arquivos ao cache
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cacheando arquivos...');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Ativa o service worker e remove caches antigos
-self.addEventListener("activate", event => {
+// Ativando o Service Worker e limpando caches antigos
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }))
-    )
-  );
-});
-
-// Intercepta requisições e serve do cache quando possível
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
     })
+  );
+});
+
+// Interceptando requisições e servindo do cache quando offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
