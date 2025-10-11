a17cbc58fe71d37cea8f67f42c4b82b2be3cad00
capelafernanda <capela.fernand4@gmail.com>
2025-10-11 08:37:30 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,44 @@
-const CACHE_NAME = 'minhas-despesas-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE_NAME = "minhas-despesas-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Instalando o Service Worker e adicionando arquivos ao cache
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cacheando arquivos...');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Ativando o Service Worker e limpando caches antigos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Cacheando arquivos...");
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        URLS_TO_CACHE.map(url =>
+          fetch(url).then(response => {
+            if (!response.ok) throw new Error(`Erro ao buscar: ${url}`);
+            return cache.put(url, response);
+          })
+        )
       );
     })
   );
 });
 
-// Interceptando requisições e servindo do cache quando offline
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
